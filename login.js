14da6af060338c7185ccee309631015c332a7920
djgon14 <djgon14pa@gmail.com>
2026-08-25 17:52:50 -0400

    Hacer que el login lleve al calendario de reservas

--- login.js
+++ login.js
@@ -6,12 +6,13 @@ const contraseñaCorrecta = "1234";
 
 loginForm.addEventListener("submit", function(event) {
     event.preventDefault();
+
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value;
 
     if (username === usuarioCorrecto && password === contraseñaCorrecta) {
         sessionStorage.setItem("usuarioAutenticado", "true");
-        window.location.href = "index.html";
+        window.location.href = "reservas.html";
     } else {
         errorMessage.textContent = "Usuario o contraseña incorrectos.";
     }
