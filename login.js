a1245dca3649a00f7b33e221e42f0516b883dc3f
djgon14 <djgon14pa@gmail.com>
2026-08-25 17:49:41 -0400

    Agregar login.js de proyecto a SIS

---
+++ login.js
@@ -0,0 +1,18 @@
+const loginForm = document.getElementById("login-form");
+const errorMessage = document.getElementById("error-message");
+
+const usuarioCorrecto = "docente";
+const contraseñaCorrecta = "1234";
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
+
+    if (username === usuarioCorrecto && password === contraseñaCorrecta) {
+        sessionStorage.setItem("usuarioAutenticado", "true");
+        window.location.href = "index.html";
+    } else {
+        errorMessage.textContent = "Usuario o contraseña incorrectos.";
+    }
+});
