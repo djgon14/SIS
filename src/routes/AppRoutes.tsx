b97c55470feb27e2686a0de25834724448d1c969
djgon14 <djgon14pa@gmail.com>
2026-08-25 17:38:52 -0400

    Mostrar el carrusel de proyecto como página principal de SIS

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,24 @@
 import {
   BrowserRouter,
+  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
 
-
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
-
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* Al entrar a SIS se muestra directamente el carrusel de proyecto */}
+        <Route path="/" element={<Navigate to="/carrusel.html" replace />} />
+        <Route path="/home-original" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default AppRoutes;
